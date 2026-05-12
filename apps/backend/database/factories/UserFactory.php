93d9b52fb2e8ab09893ac3654ddf724d654c0ed5
kimtram1 <shangheewon@gmail.com>
2026-05-12 15:03:22 +0700

    update

--- apps/backend/database/factories/UserFactory.php
+++ apps/backend/database/factories/UserFactory.php
@@ -20,7 +20,7 @@ class UserFactory extends Factory
     /**
      * Define the model's default state.
      *
-     * @return array<string, mixed>
+     * @return array<string, mixed>x`
      */
     public function definition(): array
     {
@@ -29,7 +29,6 @@ class UserFactory extends Factory
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
         ];
     }
 
