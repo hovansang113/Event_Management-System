f06a875fd87b6968cb3a6752b04488342e3af6c7
Beng-19 <beng.ho27@student.passerellesnumeriques.org>
2026-05-27 16:10:44 +0700

    fix: register brevo transport

--- apps/backend/bootstrap/providers.php
+++ apps/backend/bootstrap/providers.php
@@ -1,9 +1,11 @@
 <?php
 
 use App\Providers\AppServiceProvider;
+use App\Providers\BrevoMailServiceProvider;
 use App\Providers\RepositoryServiceProvider;
 
 return [
     AppServiceProvider::class,
     RepositoryServiceProvider::class,
+    BrevoMailServiceProvider::class,
 ];
