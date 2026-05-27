b814302d86369db42d00985fb900110e6be7bf07
Beng-19 <beng.ho27@student.passerellesnumeriques.org>
2026-05-27 16:19:17 +0700

    fix: brevo api transport

--- apps/backend/app/Providers/BrevoMailServiceProvider.php
+++ apps/backend/app/Providers/BrevoMailServiceProvider.php
@@ -4,20 +4,15 @@ namespace App\Providers;
 
 use Illuminate\Mail\MailManager;
 use Illuminate\Support\ServiceProvider;
-use Symfony\Component\Mailer\Bridge\Brevo\Transport\BrevoTransportFactory;
-use Symfony\Component\Mailer\Transport\Dsn;
 
 class BrevoMailServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
         $this->app->make(MailManager::class)->extend('brevo', function () {
-            $factory = new BrevoTransportFactory();
-            return $factory->create(new Dsn(
-                'brevo+api',
-                'default',
+            return new \Symfony\Component\Mailer\Bridge\Brevo\Transport\BrevoApiTransport(
                 config('services.brevo.key')
-            ));
+            );
         });
     }
 }
